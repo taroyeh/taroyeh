7c8fb6f3f78b5e1e14805c8bbf2d83f0ce19c83d
taroyeh <taroyeh@gmail.com>
2016-01-12 16:10:11 +0800

    Create ne_wiki_table.js

---
+++ ne_wiki_table.js
@@ -0,0 +1,61 @@
+function parse(jCode, headerTexts, regExp) {
+    var result = [];
+    var match = regExp.exec(jCode);
+    while (match != null) {
+        var obj = {};
+        for (var i = 0; i < headerTexts.length; i++) {
+            obj[headerTexts[i]] = match[i + 1];
+        }
+        result.push(obj);
+        match = regExp.exec(jCode);
+    }
+    return result;
+}
+
+function render(parsedData, headerTexts) {
+    var maxLength = {};
+    for (var i = 0; i < headerTexts.length; i++) {
+        var attr = headerTexts[i];
+        maxLength[attr] = attr.length;
+    }
+
+    for (var i in parsedData) {
+        var obj = parsedData[i];
+        for (var i = 0; i < headerTexts.length; i++) {
+            var attr = headerTexts[i];
+            if (obj[attr].length > maxLength[attr]) {
+                maxLength[attr] = obj[attr].length;
+            }
+        }
+    }
+
+    var header = "^ ";
+    for (var i = 0; i < headerTexts.length; i++) {
+        var attr = headerTexts[i];
+        header += paddingString(attr, maxLength[attr]) + " ^ ";
+    }
+    header += "\n";
+
+    var body = "";
+    for (var i in parsedData) {
+        var obj = parsedData[i];
+        var row = "| ";
+        for (var i = 0; i < headerTexts.length; i++) {
+            var attr = headerTexts[i];
+            row += paddingString(obj[attr], maxLength[attr]) + " | ";
+        }
+        body += row + "\n";
+    }
+
+    return header + body;
+}
+
+function paddingString(str, digits) {
+    var len = str.length;
+    if (digits > len) {
+        for (var i = len; i < digits; i++) {
+            str += " ";
+        }
+    }
+    return str;
+}
